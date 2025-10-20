2f7d82dc6c5db911604b648d19ade58cc09830fc
Guilherme <brunetto062006@gmail.com>
2025-10-20 17:42:08 -0300

    Corrige base para GitHub Pages com Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/presente/',  // <<=== precisa ser o mesmo nome do seu repositório
   plugins: [react()],
-})
+});
